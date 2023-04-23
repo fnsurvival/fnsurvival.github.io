9e89b0a683b6b7125d577958a69f40299fdf240e
papnineone <fnsurvival@gmail.com>
2023-04-23 22:34:12 +0700

    changed

--- js/idk.js
+++ js/idk.js
@@ -32,22 +32,24 @@ function showPage() {
 // MENU RIGHTCLICK
 document.onclick = hideMenu;
 document.oncontextmenu = rightClick;
+
 function hideMenu() {
     document.getElementById("contextMenu").style.display = "none"
 }
+
 function rightClick(e) {
     e.preventDefault();
-    if (
-        document.getElementById("contextMenu").style.display == "block"
-    )
-        hideMenu();
-    else {
-        var menu = document.getElementById("contextMenu")
-        menu.style.display = 'block';
-        menu.style.left = e.pageX + "px";
-        menu.style.top = e.pageY + "px";
-    }
-}
+
+    //  RIGHT CLICK FUNTION (( DISABLED - WONT FIX ))
+    // if (document.getElementById("contextMenu").style.display == "block") {
+    //     hideMenu();
+    // } else {
+    //     var menu = document.getElementById("contextMenu")
+    //     menu.style.display = 'block';
+    //     menu.style.left = e.pageX + "px";
+    //     menu.style.top = e.pageY + "px";
+    // }
+} 
 
 // PREVENT CTRL
 var isNS = (navigator.appName == "Netscape") ? 1 : 0;
